3588fe2a3ae3f21480062ba1d0fd2dbaaf359b75
tanvi <tanvimeer.tops@gmail.com>
2022-08-08 18:58:32 +0530

    inheritance

---
+++ Inheritance.cpp
@@ -0,0 +1,36 @@
+/* inheritance 
+creating a new file from an existing file 
+advantage 
+redundancy of code is less 
+object will be created for child class
+types inheritance
+1. Single (class A : parent,base,super)(class b : child,sub,inherit,derived)
+2. multilevel
+3. multiple (ambiquity)
+4. hierarchical
+5. hybird
+*/
+//single inheritance
+#include<iostream>
+#include "People.h"
+using namespace std;
+class Student:public people{
+private:
+int fees;
+public:
+void set_fees(){
+    cout<<"enter fees:"<<endl;
+    cin>>fees;
+}
+void get_fees(){
+    cout<<fees<<endl;
+}
+};
+
+int main(){
+Student s;
+s.set_data();
+s.get_data();
+s.set_fees();
+s.get_fees();
+}
---
+++ People.h
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class people{
+private:
+int id;
+char name[100];
+public:
+void set_data(){
+    cout<<"Enter name:"<<endl;
+    cin>>name;
+    cout<<"enter id:"<<endl;
+    cin>>id;
+}
+void get_data(){
+cout<<name<<endl;
+cout<<id<<endl;
+}
+};
