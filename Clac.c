ee0b88315917974cb2a40d7df3993d4830f58fc4
tanvimeertops <109589213+tanvimeertops@users.noreply.github.com>
2023-01-05 08:20:33 +0530

    Add files via upload
    gdfgdf

---
+++ Clac.c
@@ -0,0 +1,17 @@
+//operators: 
+//arithmetic operator : +,-,*,/,%
+/*
+type casting : 
+implicit: small size to big size (automatic)
+explicit : big to small 
+*/
+#include<stdio.h>
+void main(){
+int a=10,b=3;//
+printf("\naddition is %d",a+b);
+printf("\nsub is %d",a-b);
+printf("\nmul is %d",a*b);
+//float c=(float)a/b;
+printf("\ndiv is %f",(float)a/b);
+printf("\nmodulus is %d",a%b);
+}
